108cd44d32a96bae0fe143b946c66db8e9e2331e
abhiram <abhiramps776@gmail.com>
2022-04-20 15:10:49 +0400

    added crossOrigin="true" in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           
           {/* google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Glory:wght@600;700&family=Nunito+Sans:ital,wght@0,200;1,400&family=Open+Sans&family=Rubik:wght@500&display=swap" rel="stylesheet" />
         </Head>
 
